e2a7c37541f0924d6d96255a312d8fbaa376b181
ullas y <111691611+ullas-y@users.noreply.github.com>
2022-08-31 17:17:06 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,29 +1,33 @@
 import os
-import time
 
 board = [' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ']
-
-###################
 win = 1
 draw = -1
 running = 0
-###########################
 game = running
 
+
+def initialise_data():
+    global board
+    global win
+    global draw
+    global running
+    global game 
+    board = [' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ']
+    win = 1
+    draw = -1
+    running = 0
+    game = running
 
 
 # This Function Draws Game Board
 def draw_board():
-    print(" %c| %c | %c " % (board[1], board[2], board[3]))
+    print(" %c | %c | %c " % (board[1], board[2], board[3]))
     print("___|___|___")
     print(" %c | %c | %c " % (board[4], board[5], board[6]))
     print("___|___|___")
     print(" %c | %c | %c " % (board[7], board[8], board[9]))
     print("   |   |   ")
-
-
-# This Function Checks position is empty or not
-
 
 
 # This Function Checks player has won or not
@@ -50,45 +54,51 @@ def check_win():
         game = win
         # Match Tie or Draw Condition
     elif board[1] != ' ' and board[2] != ' ' and board[3] != ' ' and board[4] != ' ' and board[5] != ' ' and board[
-        6] != ' ' and board[7] != ' ' and board[8] != ' ' and board[9] != ' ':
+            6] != ' ' and board[7] != ' ' and board[8] != ' ' and board[9] != ' ':
         game = draw
     else:
         game = running
 
 
+def con():
+    inp = input("Do you want to continue y or n: ")
+    if inp == 'y' or inp == 'Y':
+        initialise_data()
+        main()
+    else:
+        print('Exiting')
+        exit()
+
+
+def check_position(x):
+    if board[x] == ' ':
+        return True
+    else:
+        return False
+
+
 def main():
-    draw_board()
-    check_win()
     player = 1
-
-    def check_position(x):
-        if board[x] == ' ':
-            return True
-        else:
-            return False
-
     print("Tic-Tac-Toe Game ")
-    print("Player 1 [X] --- Player 2 [O]\n")
-    print()
-    print()
-    print("Please Wait...")
-    time.sleep(3)
+    print("Player 1 [X] --- Player 2 [O]\n\n")
+    
     while game == running:
-        os.system('cls')
         draw_board()
         if player % 2 != 0:
-            print("Player 1's chance")
+            print("\nPlayer 1's chance")
             mark = 'X'
         else:
-            print("Player 2's chance")
+            print("\nPlayer 2's chance")
             mark = 'O'
-        choice = int(input("Enter the position between [1-9] where you want to mark : "))
+        choice = int(
+            input("\nEnter the position between [1-9] where you want to mark : "))
         if check_position(choice):
             board[choice] = mark
             player += 1
             check_win()
+        os.system('clear')
 
-    os.system('cls')
+    os.system('clear')
     draw_board()
     if game == draw:
         print("Game Draw")
@@ -99,14 +109,7 @@ def main():
         else:
             print("Player 2 Won")
 
-    def con():
-        inp = input("do u want to continue y or no")
-        if inp == 'y' or 'Y':
-            main()
-        else:
-            exit()
     con()
 
+
 main()
-
-
